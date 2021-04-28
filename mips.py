6e79d2fcee81f33521c5634b26a94db25b9fe5cc
pinexmas <pinexmas@gmail.com>
2021-04-28 15:48:14 -0700

    Init commit

---
+++ mips.py
@@ -0,0 +1,10 @@
+class Parser:
+    """
+    This class provides methods to parse MIPS codes in hex to instructions
+    """
+
+    def __init__(self):
+        print('A Parser obj is created')
+
+    def parse(self, s: str):
+        print('Input string = ', s)
